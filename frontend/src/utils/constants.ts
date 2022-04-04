817d78dfd68683c016e35e9cc478c7321dccb64c
venkatesh-elchuri <venkatesh.elchuri@hpe.com>
2022-04-04 07:03:35 +0000

    typo..!

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://52.91.220.1/:3001';
+export const baseUrl = 'http://52.91.220.1:3001';
 export const GET_USER_LIST = baseUrl + '/user/list';
 export const CREATE_USER_INFO = baseUrl + '/user/create';
 export const UPDATE_USER_INFO = baseUrl + '/user/update';
